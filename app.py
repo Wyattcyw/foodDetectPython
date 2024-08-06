1e51d90b1e7a94092484eeac92558f39ede3285a
LTA_Intern_Wyatt_CLI <2100808A@student.tp.edu.sg>
2024-08-06 16:32:33 +0800

    asjfdoidsj

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import numpy as np
 
 app = Flask(__name__)
 CORS(app)
+model = YOLO("best.pt")
 
 DETECTION_URL = "/yolov5"
 IMAGE_URL = "/image"
@@ -68,6 +69,6 @@ def predict():
     else:
         return jsonify({"error": "No image file uploaded"}), 400  # Explicitly return 400 for bad request
 
-if __name__ == "__main__":
-    model = YOLO("best.pt")
-    # app.run(host="0.0.0.0")
+# if __name__ == "__main__":
+#     model = YOLO("best.pt")
+#     # app.run(host="0.0.0.0")
